351378c93c2f4b5863576cbe500ff7656b933c12
Dimitris Kaitantzidis <james_k23@hotmail.gr>
2022-09-14 13:50:00 +0300

    Adding springdatajpa as profile to app.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PersonMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PersonMapService.java
@@ -2,11 +2,13 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Person;
 import guru.springframework.sfgpetclinic.services.CrudService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class PersonMapService extends AbstractMapService<Person, Long> implements CrudService<Person, Long> {
     @Override
     public Set<Person> findAll() {
